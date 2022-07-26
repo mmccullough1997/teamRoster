d991ac7f863165859525bad0b62af523f29f8b55
Mitch McCullough <mitchmccullough@gmail.com>
2022-07-26 15:07:58 -0500

    team and player forms

--- pages/team/new.js
+++ pages/team/new.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import TeamForm from '../../components/forms/TeamForm';
 // import TeamForm from '../../components/forms/TeamForm';
 
 export default function AddTeam() {
   return (
-    <h1>Hello</h1>
+    <TeamForm />
   );
 }
