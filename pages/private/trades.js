f792e40e5cb644373a895acb630a4a023ad75d2d
Mitch McCullough <mitchmccullough@gmail.com>
2022-07-29 15:39:04 -0500

    took search off trades

--- pages/private/trades.js
+++ pages/private/trades.js
@@ -2,21 +2,18 @@
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { Button } from 'react-bootstrap';
-import Search from '../../components/Search';
 import { getPrivateTrades } from '../../api/tradeData';
 import { useAuth } from '../../utils/context/authContext';
 import TradeCard from '../../components/TradeCard';
 
 export default function Trades() {
   const [trades, setTrades] = useState([]);
-  const [filteredTrades, setFilteredTrades] = useState([]);
   const { user } = useAuth();
   // console.warn(user.uid);
 
   const getAllTrades = () => {
     getPrivateTrades(user.uid).then((theTrades) => {
       setTrades(theTrades);
-      setFilteredTrades(theTrades);
     });
   };
 
@@ -31,10 +28,7 @@ export default function Trades() {
         <Link passHref href="/trade/newTrade">
           <Button>New Trade</Button>
         </Link>
-      </div>
-      <Search players={trades} setFilteredPlayers={setFilteredTrades} onUpdate={getAllTrades} />
-      <div className="d-flex flex-wrap">
-        {filteredTrades.map((trade) => (
+        {trades.map((trade) => (
           <TradeCard key={trade.firebaseKey} tradeObj={trade} onUpdate={getAllTrades} />
         ))}
       </div>
